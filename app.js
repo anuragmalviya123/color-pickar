7c951c5d12f99db8c951bdb728150006c610c501
anuragmalviya123 <anuragmalviya62@gmail.com>
2023-10-16 13:43:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+function handleclick(){
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+}
+
+btn.addEventListener("click", handleclick);
+
+
+// btn.addEventListener("click", function () {
+//   const randomNumber = getRandomNumber();
+//   document.body.style.backgroundColor = colors[randomNumber];
+//   color.textContent = colors[randomNumber];
+// });
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
